ea236f947a2b68a65f12d9367ac3a06b4a341694
Alireza Aghili <siavashaghili@gmail.com>
2023-09-14 22:42:17 +0100

    build() added with all new tests

--- src/Container.php
+++ src/Container.php
@@ -3,6 +3,7 @@
 namespace Siavash\Container;
 
 use Psr\Container\ContainerInterface;
+use Siavash\Container\Exceptions\CouldNotResolveCLassException;
 
 class Container implements ContainerInterface
 {
@@ -26,19 +27,49 @@ class Container implements ContainerInterface
 
     }
 
-    public function get(string $id)
+    public function get(string $service)
     {
-        $service = $this->services[$id];
-
-        if ($service instanceof \Closure) {
-            return $service();
+        if ($this->has($service)) {
+            $service = $this->services[$service];
+            if ($service instanceof \Closure) {
+                return $service();
+            }
+            return $service;
         }
-
-        return  $service;
+        if (class_exists($service)) {
+             return $this->build($service);
+        }
+       throw new CouldNotResolveCLassException();
     }
 
     public function has(string $id): bool
     {
         return array_key_exists($id, $this->services);
+    }
+
+    protected function dd(...$args)
+    {
+        var_dump(...$args);
+        die;
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    protected function build(string $service): object
+    {
+       $reflector = new \ReflectionClass($service);
+       $parameters = $reflector->getConstructor()?->getParameters() ?? [];
+
+       $resolveDependencies = array_map(function (\ReflectionParameter $parameter) {
+           $class = $parameter->getType()->getName();
+
+           if (class_exists($class)) {
+               return $this->build($class);
+           }
+       }, $parameters);
+
+//       $this->dd($resolveDependencies);
+        return $reflector->newInstanceArgs($resolveDependencies);
     }
 }
---
+++ src/Exceptions/CouldNotResolveCLassException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Siavash\Container\Exceptions;
+
+use Psr\Container\NotFoundExceptionInterface;
+
+class CouldNotResolveCLassException extends \RuntimeException implements NotFoundExceptionInterface
+{
+
+
+}
